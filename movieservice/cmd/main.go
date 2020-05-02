b36a8e6eb84a47a24a65b43dbff76a5794827db9
reynaldipane <reynaldi@xendit.co>
2020-05-02 20:11:20 +0700

    Add projects config

--- movieservice/cmd/main.go
+++ movieservice/cmd/main.go
@@ -10,6 +10,7 @@ import (
 	"syscall"
 
 	proto "github.com/reynaldipane/microservice-test/movieservice/pb"
+	conf "github.com/reynaldipane/microservice-test/movieservice/pkg/configs"
 	endpoints "github.com/reynaldipane/microservice-test/movieservice/pkg/movieendpoint"
 	services "github.com/reynaldipane/microservice-test/movieservice/pkg/movieservice"
 	transports "github.com/reynaldipane/microservice-test/movieservice/pkg/movietransport"
@@ -18,22 +19,23 @@ import (
 
 func main() {
 	ctx := context.Background()
+	conf := conf.Config{}
+
 	var svc services.Service
 
-	// svc = services.MovieService{}
-	svc = services.NewMovieService(http.DefaultClient, "http://www.omdbapi.com", "faf7e5bb")
+	svc = services.NewMovieService(http.DefaultClient, conf.Get("OMDB_API_BASE_URL"), conf.Get("OMDB_API_KEY"))
 	errChan := make(chan error)
 
 	endpoints := endpoints.Set{
 		FindMoviesEndpoint: endpoints.New(svc).FindMoviesEndpoint,
 	}
 	go func() {
-		listener, err := net.Listen("tcp", ":4042")
+		listener, err := net.Listen("tcp", fmt.Sprintf(":%s", conf.Get("PORT")))
 		if err != nil {
 			errChan <- err
 			return
 		} else {
-			fmt.Println("Server started on :4042")
+			fmt.Printf("Server started on :%s", conf.Get("PORT"))
 		}
 		handler := transports.NewGRPCServer(ctx, endpoints)
 		grpcServer := grpc.NewServer()
